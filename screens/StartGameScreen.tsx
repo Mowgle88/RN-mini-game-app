b71e0a3e82fbfebc0e1f953a5f874f3ca7147622
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-15 00:20:46 +0300

    feat: add primaryBurron component

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,17 +1,58 @@
 import React from 'react'
 import { StyleSheet, TextInput, View, Text } from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
 
 export default function StartGameScreen() {
 
-
   return (
-    <View>
-      <Text>StartGameScreen</Text>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4e0329',
+    borderRadius: 8,
+    elevation: 8,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    marginVertical: 8,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  buttonsContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
+  }
 })
