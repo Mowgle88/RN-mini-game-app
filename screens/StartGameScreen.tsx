fdfe17708fd9efd8f9a328fb4dbf844f6d6b04a3
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-14 21:34:30 +0300

    feat: add screen layouts

---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, TextInput, View, Text } from 'react-native'
+
+export default function StartGameScreen() {
+
+
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+      <TextInput />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
