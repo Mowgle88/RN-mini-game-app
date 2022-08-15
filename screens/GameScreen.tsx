80c1c20ed764ae23c02ae9e4e618ab2a57698ce5
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-16 00:11:29 +0300

    feat: add random number to number-container component

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import Title from '../components/Title'
+import NumberContainer from '../components/game/NumberContainer';
+import Title from '../components/ui/Title'
 
-export default function GameScreen() {
+interface GameScreenProps {
+  userNumber: number
+}
+
+function generateRandomBetween(min: number, max: number, exclude: number): number {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+export default function GameScreen({ userNumber }: GameScreenProps) {
+
+  const initialGuess = generateRandomBetween(1, 100, userNumber)
+
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
     </View>
   )
 }
