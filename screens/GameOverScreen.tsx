a13e9a5e935bfe268a89a525d2b334723299f078
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-17 22:24:13 +0300

    feat: add logic to restart game and displaying a summary screen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { StyleSheet, View, Image } from 'react-native'
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
-import Title from '../components/ui/Title'
-import Colors from '../constants/colors'
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
-export default function GameOverScreen() {
+interface GameOverScreenProps {
+  roundsNumber: number,
+  userNumber: number,
+  onStartNewGame: () => void
+}
+
+export default function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }: GameOverScreenProps) {
   return (
     <View style={styles.rootContainer}>
       <Title>GAME OVER</Title>
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require('../assets/images/success.png')} />
       </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text>
+      </Text>
+      <PrimaryButton onPress={onStartNewGame} >Start New Game</PrimaryButton>
     </View>
   )
 }
@@ -34,5 +45,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500
   }
 })
