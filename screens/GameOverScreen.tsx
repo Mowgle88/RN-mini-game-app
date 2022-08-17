923cb4615e1515977e5c337edc3f2dc6207590af
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-17 21:32:40 +0300

    feat: add foreground image and title to game-over-screen component

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
+
+import Title from '../components/ui/Title'
+import Colors from '../constants/colors'
 
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../assets/images/success.png')} />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    borderRadius: 150,
+    overflow: 'hidden',
+    margin: 36
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
 })
