7d73b1079d1f60c3368c17e636c7be9f99daf90c
Mowgle88 <mowgle1988@gmail.com>
2022-08-20 17:28:34 +0300

    feat: add managing Screen Content with KeyboardAvoidingView

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, TextInput, View, Alert, useWindowDimensions } from 'react-native'
+import { StyleSheet, TextInput, View, Alert, useWindowDimensions, KeyboardAvoidingView, ScrollView } from 'react-native'
 import Card from '../components/ui/Card'
 import InstructionText from '../components/ui/InstructionText'
 import PrimaryButton from '../components/ui/PrimaryButton'
@@ -40,32 +40,38 @@ export default function StartGameScreen({ onPickNumber }: StartGameScreenProps) 
   const marginTopDistance = height < 400 ? 30 : 100;
 
   return (
-    <View style={[styles.screenContainer, { marginTop: marginTopDistance }]}>
-      <Title>Guess my Number</Title>
-      <Card>
-        <InstructionText>Enter a Number</InstructionText>
-        <TextInput
-          style={styles.numberInput}
-          maxLength={2}
-          keyboardType='number-pad'
-          onChangeText={numberInputHandler}
-          value={enterNumber}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-          </View>
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView style={styles.screen} behavior='position'>
+        <View style={[styles.screenContainer, { marginTop: marginTopDistance }]}>
+          <Title>Guess my Number</Title>
+          <Card>
+            <InstructionText>Enter a Number</InstructionText>
+            <TextInput
+              style={styles.numberInput}
+              maxLength={2}
+              keyboardType='number-pad'
+              onChangeText={numberInputHandler}
+              value={enterNumber}
+            />
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+              </View>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+              </View>
+            </View>
+          </Card>
         </View>
-      </Card>
-    </View>
-
+      </KeyboardAvoidingView>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
   screenContainer: {
     flex: 1,
     marginTop: 100,
